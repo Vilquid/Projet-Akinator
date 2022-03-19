f25f7d2f8a74effa2719e876fe227e4ae2a4aa77
Mathis <mathis.de-gueydon@isen.yncrea.fr>
2022-03-19 15:48:00 +0100

    Ajout de la fonction qui affiche AKINATOR

--- main.c
+++ main.c
@@ -1,12 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
-
 #include "structs-functions.h"
 
 
 int main(int argc, char const *argv[])
 {
-	printf("cc");
-
+	akinator();
 	return 0;
 }
--- structs-functions.h
+++ structs-functions.h
@@ -1,12 +1,15 @@
 #ifndef PROJET_1___AKINATOR_STRUCTS_FUNCTIONS_H
 #define PROJET_1___AKINATOR_STRUCTS_FUNCTIONS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 // Placer les déclarations des structures après ce commentaire
 
 // Placer les déclarations des structures avant ce commentaire
 
 // Placer les prototypes des fonctions après ce commentaire
-
+void akinator();
 // Placer les prototypes des fonctions avant ce commentaire
 
 #endif //PROJET_1___AKINATOR_STRUCTS_FUNCTIONS_H
