311812a202ca638ea25a1f5d6513c52e53ba6b5a
Mathis <mathis.de-gueydon@isen.yncrea.fr>
2022-03-20 12:51:52 +0100

    Summary
    Flemme d'écrire un truc

--- structs-functions.c
+++ structs-functions.c
@@ -2,7 +2,8 @@
 
 
 // Déclarer les fonctions après ce commentaire
-void akinator()
+
+void akinator() // ne pas toucher même si c'est moche car ça rend bien
 {
 	printf("  .--.  .-. .-..-..-. .-.  .--.  .---.  .----. .----.\n");
 	printf(" / {} \\ | |/ / | ||  `| | / {} \\{_   _}/  {}  \\| {}  }\n");
