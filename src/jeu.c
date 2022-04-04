7c47ce21c9cc8ea1cbb57edeee37647d4d49a6a5
ZeNico13 <44881945+ZeNico13@users.noreply.github.com>
2022-04-04 16:35:46 +0200

    Ajout fonction suppression suspects selon âge

--- src/jeu.c
+++ src/jeu.c
@@ -29,3 +29,22 @@ int calculAgeMoyenHerosSuspects(Liste_Heros *liste_heros) {
 
 	return (int)(age_moyen / nb_heros);
 }
+
+/**
+ * @brief Fonction permettant de supprimer des suspects les héros qui ont un âge supérieur à la moyenne des héros suspects restants
+ * @param liste_heros Liste des héros
+**/
+void supprimerHerosSuspectsSelonAge(Liste_Heros *liste_heros) {
+
+	Elt_Heros *elt_actuel = liste_heros->premier;
+	int age_moyen = calculAgeMoyenHerosSuspects(liste_heros);
+
+	while (elt_actuel != NULL)
+	{
+		if (elt_actuel->heros->age > age_moyen) {
+			elt_actuel->suspect = false;
+		}
+
+		elt_actuel = elt_actuel->suivant;
+	}
+}
