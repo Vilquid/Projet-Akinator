9f16a305b34b7fbff2ed071a36f772384df3cfe2
ZeNico13 <44881945+ZeNico13@users.noreply.github.com>
2022-04-05 10:10:05 +0200

    Ajout fonctions suppression suspects
    - Ajout fonction `calculAgeMoyenHeros()`
    - Ajout fonction `supprimerHerosSelonAge()`
    - Ajout fonction `supprimerHerosSelonSexe()`
    - Ajout fonction `supprimerHerosSelonNationalite()`
    - Ajout fonction `supprimerHerosSelonEspece()`
    - Ajout fonction `supprimerHerosSelonEquipement()`
    - Ajout fonction `supprimerHerosSelonVolant()`
    - Ajout fonction `supprimerHerosSelonMasque()`
    - Ajout fonction `supprimerHerosSelonTaille()`
    - Ajout fonction `supprimerHerosSelonCouleur()`
    
    - Ajout fonction `afficherHeros()` qui affiche tous les attributs du héros passé en paramètre
    
    - Temps passé : Trop pas grand chose

--- include/jeu.h
+++ include/jeu.h
@@ -18,8 +18,19 @@
    Déclaration des Fonctions
 -----------------------------*/
 
-int calculAgeMoyenHerosSuspects(Liste_Heros *liste);
-void supprimerHerosSuspectsSelonAge(Liste_Heros *liste);
 Question *choisirQuestionAleatoire(Liste_Questions *liste);
+void afficherHeros(Heros *heros);
+
+int calculAgeMoyenHeros(Liste_Heros *liste);
+
+void supprimerHerosSelonAge(Liste_Heros *liste);
+void supprimerHerosSelonSexe(Liste_Heros *liste, Sexe sexe);
+void supprimerHerosSelonNationalite(Liste_Heros *liste, char nationalite[25]);
+void supprimerHerosSelonEspece(Liste_Heros *liste, Espece espece);
+void supprimerHerosSelonEquipement(Liste_Heros *liste, bool equipement);
+void supprimerHerosSelonVolant(Liste_Heros *liste, bool volant);
+void supprimerHerosSelonMasque(Liste_Heros *liste, bool masque);
+void supprimerHerosSelonTaille(Liste_Heros *liste, bool taille);
+void supprimerHerosSelonCouleur(Liste_Heros *liste, char couleur[25]);
 
 #endif // JEU_H
